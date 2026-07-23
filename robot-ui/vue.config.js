05cc0a1dd94d5625f63999376f992a059616ee7a
13990780790 <5151456+pioneerfy@user.noreply.gitee.com>
2026-07-23 15:38:34 +0800

    配置项

--- robot-ui/vue.config.js
+++ robot-ui/vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
   // 部署生产环境和开发环境下的URL。
   // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
   // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
   outputDir: 'dist',
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
@@ -52,12 +52,10 @@ module.exports = {
     //   cert: fs.readFileSync('./server.crt'),
     // },
     proxy: {
-      ['/dev-api']: {
-        // target: process.env.VUE_APP_BASE_IP,
-        // target: 'http://192.168.124.204:8181',
-        // target: 'http://192.168.124.77:8090/',
-        target: 'https://192.168.124.77:4443/',
-        // target: 'https://10.2.75.230:443',
+      '/dev-api': {
+        target: process.env.VUE_APP_BASE_ORIGIN,
+        // target: 'https://192.168.124.77:4443/',
+        // target: 'https://192.168.124.234:4443/',
         changeOrigin: true,
         // secure: true,
         // secure: false,
@@ -66,20 +64,20 @@ module.exports = {
           console.log('代理请求:', req.method, req.url)
         },
         onError(err, req, res) {
-          console.log('代理错误:', err);
+          console.log('代理错误:', err)
         },
         pathRewrite: {
           ['^' + '/dev-api']: ''
-        },
+        }
       },
-      ['/ws']: {
+      '/ws': {
         // target: process.env.VUE_APP_BASE_IP,
         // target: 'http://192.168.124.204:8181',
         // target: 'http://192.168.124.77:8090/',
         target: 'wss://192.168.124.77:4443/',
         ws: true,
         // target: 'https://10.2.75.230:443',
-        changeOrigin: true,
+        changeOrigin: true
       }
     },
     disableHostCheck: true
@@ -87,7 +85,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        sassOptions: { outputStyle: "expanded" }
+        sassOptions: { outputStyle: 'expanded' }
       }
     }
   },
@@ -111,15 +109,15 @@ module.exports = {
     plugins: [
       // http://doc.ruoyi.vip/ruoyi-vue/other/faq.html#使用gzip解压缩静态文件
       new CompressionPlugin({
-        cache: false,                                  // 不启用文件缓存
-        test: /\.(js|css|html|jpe?g|png|gif|svg)?$/i,  // 压缩文件格式
-        filename: '[path][base].gz[query]',            // 压缩后的文件名
-        algorithm: 'gzip',                             // 使用gzip压缩
-        minRatio: 0.8,                                 // 压缩比例，小于 80% 的文件不会被压缩
-        deleteOriginalAssets: false                    // 压缩后删除原文件
-      }),
+        cache: false, // 不启用文件缓存
+        test: /\.(js|css|html|jpe?g|png|gif|svg)?$/i, // 压缩文件格式
+        filename: '[path][base].gz[query]', // 压缩后的文件名
+        algorithm: 'gzip', // 使用gzip压缩
+        minRatio: 0.8, // 压缩比例，小于 80% 的文件不会被压缩
+        deleteOriginalAssets: false // 压缩后删除原文件
+      })
       // new BundleAnalyzerPlugin()
-    ],
+    ]
     // optimization: process.env.NODE_ENV === 'production' ? {
     //   minimize: true,
     //   minimizer: [
@@ -174,39 +172,39 @@ module.exports = {
       .end()
 
     config.when(process.env.NODE_ENV !== 'development', config => {
-          config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
-              inline: /runtime\..*\.js$/
-            }])
-            .end()
+      config
+        .plugin('ScriptExtHtmlWebpackPlugin')
+        .after('html')
+        .use('script-ext-html-webpack-plugin', [{
+          // `runtime` must same as runtimeChunk name. default is `runtime`
+          inline: /runtime\..*\.js$/
+        }])
+        .end()
 
-          config.optimization.splitChunks({
-            chunks: 'all',
-            cacheGroups: {
-              libs: {
-                name: 'chunk-libs',
-                test: /[\\/]node_modules[\\/]/,
-                priority: 10,
-                chunks: 'initial' // only package third parties that are initially dependent
-              },
-              elementUI: {
-                name: 'chunk-elementUI', // split elementUI into a single package
-                test: /[\\/]node_modules[\\/]_?element-ui(.*)/, // in order to adapt to cnpm
-                priority: 20 // the weight needs to be larger than libs and app or it will be packaged into libs or app
-              },
-              commons: {
-                name: 'chunk-commons',
-                test: resolve('src/components'), // can customize your rules
-                minChunks: 3, //  minimum common number
-                priority: 5,
-                reuseExistingChunk: true
-              }
-            }
-          })
-          config.optimization.runtimeChunk('single')
+      config.optimization.splitChunks({
+        chunks: 'all',
+        cacheGroups: {
+          libs: {
+            name: 'chunk-libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial' // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: 'chunk-elementUI', // split elementUI into a single package
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/, // in order to adapt to cnpm
+            priority: 20 // the weight needs to be larger than libs and app or it will be packaged into libs or app
+          },
+          commons: {
+            name: 'chunk-commons',
+            test: resolve('src/components'), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true
+          }
+        }
+      })
+      config.optimization.runtimeChunk('single')
     })
   }
 }
